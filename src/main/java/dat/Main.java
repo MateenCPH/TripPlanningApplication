48ef09a8e12ed57aea7f94728dc881e0c606beda
MateenCPH <cph-mr645@cphbusiness.dk>
2024-11-06 15:00:23 +0100

    minor

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,10 +1,12 @@
 package dat;
 
 import dat.config.ApplicationConfig;
+import dat.utils.Populator;
 
 public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(9009);
+        Populator.populate();
     }
 }
